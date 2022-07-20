ed5dcfd6b2c9a60e946926189f5560290710369a
quanbenn <77028868+quanbenn@users.noreply.github.com>
2022-07-20 19:00:08 -0400

    Update App.js
    Update to App.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,81 @@
-import React, {useRef} from 'react';
-import NumberButtons from './NumberButtons';
-import OperatorButtons from './OperatorButtons';
+import React, {useState,useRef} from 'react';
 
 function App() {
 
   //Variables
   const input = useRef();
+  const [answer,updateAnswer] = useState(0) //Have "updateAnswer" always work with previous answer/ previous state(?)
+  const [operator,updateOperator] = useState()
+  const [items,addItems] = useState([])
 
   //Functions
-  /*function typeNumber(){
+  function typeNumber(n){
 
-    const number = input.current.value;
+    const number = Number(input.current.value);
 
     if(typeof number != 'number'){
+      console.log(input.current.value);
       input.current.value = 'Enter a Number Please';
     }
     else if(typeof number === 'number'){
-      if(){
-
+      if(n === 1){
+        const currentNum = '' + number + 1
+        input.current.value = currentNum
       }
-      if(){
-
+      if(n === 2){
+        const currentNum = '' + number + 2
+        input.current.value = currentNum
       }
-      if(){
-
+      if(n === 3){
+        const currentNum = '' + number + 3
+        input.current.value = currentNum
       }
     }
 
-  }*/
+  }
+
+  function updateExpression(){
+    addItems(prev => [...prev,"Hello"]);
+  }
+
+  function giveAnswer(n){
+
+    if(n === '+'){
+      updateOperator(n)
+      updateAnswer(answer + Number(input.current.value))
+      input.current.value = 0
+    }
+    if(n === '-'){
+      updateOperator(n)
+      updateAnswer(Number(input.current.value) - answer)
+      input.current.value = 0
+    }
+    if(n === '/'){
+      updateOperator(n)
+      updateAnswer(Number(input.current.value)/answer)
+      input.current.value = 0
+    }
+    if(n === '*'){
+      updateOperator(n)
+      updateAnswer(answer * Number(input.current.value))
+      input.current.value = 0
+    }
+  }
 
   return (
     <>
     <input ref={input} placeholder={0}></input>
-    <NumberButtons number={1}/>
-    <NumberButtons number={2}/>
-    <NumberButtons number={3}/>
-    <OperatorButtons operator={'+'}/>
-    <OperatorButtons operator={'-'}/>
-    <OperatorButtons operator={'/'}/>
-    <OperatorButtons operator={'*'}/>
+    <button onClick={() => typeNumber(1)}>1</button>
+    <button onClick={() => typeNumber(2)}>2</button>
+    <button onClick={() => typeNumber(3)}>3</button>
+    <button onClick={() => giveAnswer('+')}>+</button>
+    <button onClick={() => giveAnswer('-')}>-</button>
+    <button onClick={() => giveAnswer('/')}>/</button>
+    <button onClick={() => giveAnswer('*')}>*</button>
+    <button onClick={() => updateExpression()}>=</button>
+    <span>{answer}</span>
+    <span>{operator}</span>
+    <span>{items}</span>
     </>
   )
 }
